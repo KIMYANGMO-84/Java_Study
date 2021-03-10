25f04066a5d5a8f9fd169738a2fca387835ebcb7
KIM YANG MO <yangdol2@nate.com>
2021-03-11 00:04:49 +0900

    반복문연습

--- Java_Study/src/Basic_Study/For_Ex4.java
+++ Java_Study/src/Basic_Study/For_Ex4.java
@@ -8,17 +8,17 @@ package Basic_Study;
 public class For_Ex4 {
 	public static void main(String[] args) {
 		String x = " ";
-		String x1 = "";
 		String y = "*";
 		String y1 = "";
 		
 		for(int i = 1;i<=4;i++) {
 			for(int j = 3;j>=i;j--) {
-				x1 = x1 + x;
+				System.out.print(x);
 			}
-				y1 = y1 + y;
-			System.out.println(x1 + y1);
-			x1 = "";
+			y1 = y1 + y;
+			System.out.print(y1);
+			System.out.println();
+			
 		}
 		
 	}
